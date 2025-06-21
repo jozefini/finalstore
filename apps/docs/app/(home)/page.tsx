41a3593a374e8188ee4c67d9752416233b17aaeb
jozefini <jozibashaj@gmail.com>
2025-06-21 09:54:58 +0200

    fix with notify

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -3,6 +3,12 @@ import Link from 'next/link';
 import { AsyncSection } from './components/ui/async-section';
 import { EventSystemSection } from './components/ui/event-system-section';
 import { HeroSection } from './components/ui/hero-section';
+import {
+  AsyncTest,
+  BasicStoreTest,
+  BatchingTest,
+  PerformanceTest
+} from './components/ui/store-test';
 import { ThreeWaySection } from './components/ui/three-way-section';
 
 export default function HomePage() {
@@ -13,6 +19,13 @@ export default function HomePage() {
       <ThreeWaySection />
       <AsyncSection />
       <EventSystemSection />
+
+      <div className="mx-auto mt-20 grid w-full max-w-2xl gap-y-10">
+        <BasicStoreTest />
+        <BatchingTest />
+        <AsyncTest />
+        <PerformanceTest />
+      </div>
 
       <div className="mb-10 mt-20 text-center">
         <Link
