9dfb7434e5192e05ce31804b5c435cd85514518a
jozefini <jozibashaj@gmail.com>
2025-06-23 22:25:27 +0200

    Update homepage

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -20,7 +20,7 @@ export default async function Page(props: {
 
   return (
     <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
+      {/* <DocsTitle>{page.data.title}</DocsTitle> */}
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
         <MDX components={{ ...defaultMdxComponents } as MDXComponents} />
